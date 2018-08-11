28e729be0e3a5705c53610c6f6f4f4d29b899579
AdrienPoupa <adrien@poupa.fr>
2018-08-11 19:02:56 -0400

    Remove promotion from factory

--- database/factories/OrdersFactory.php
+++ database/factories/OrdersFactory.php
@@ -5,7 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Order::class, function (Faker $faker) {
     return [
         'customer' => $faker->text(10),
-        'promotion' => $faker->text(10),
         'price'=> $faker->randomFloat(2,5, 10),
         'street_number'=> $faker->randomFloat(0,1, 100),
         'street_name'=> $faker->text(10),
