e03670ca78dd2c8a3ff3fee33780c7dea1349d22
AdrienPoupa <adrien@poupa.fr>
2018-08-11 19:49:49 -0400

    Remove amount from factory

--- database/factories/OrderItemToppingFactory.php
+++ database/factories/OrderItemToppingFactory.php
@@ -5,7 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\OrderItemTopping::class, function (Faker $faker) {
     return [
         'order_item_id'  => $faker->numberBetween(1, 40),
-        'topping_id'  => $faker->numberBetween(1, 10),
-        'amount'    => $faker->numberBetween(1, 5)
+        'topping_id'  => $faker->numberBetween(1, 10)
     ];
 });
