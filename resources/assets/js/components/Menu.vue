a2e7dd84d87799833629f90eda67d7c7effb98c4
Ksenia Popova <p.ksyusha@gmail.com>
2018-08-12 19:20:30 -0400

    Some changes

--- resources/assets/js/components/Menu.vue
+++ resources/assets/js/components/Menu.vue
@@ -13,7 +13,7 @@
                   <p class="text text-left pl-2 pr-2 mb-2">{{ $t('bagel.' + bagel.id + '.description') }}</p>
                   <div class="row no-gutters">
                     <div class="col-6">
-                      <p class="calories mb-2 pl-2">Calories : {{ bagel.calories }}</p>
+                      <p class="calories mb-2 pl-2">{{ $t('calories')}}: {{bagel.calories }}</p>
                     </div>
                     <div class="col-6">
                       <button @click.prevent="addToCart(bagel);toast($t('bagel.' + bagel.id + '.title'));">Add</button>
@@ -34,7 +34,7 @@
                 <p class="text text-left pl-2 pr-2 mb-2">{{ $t('drink.' + drink.id + '.description') }}</p>
                 <div class="row no-gutters">
                   <div class="col-6">
-                    <p class="calories mb-2 pl-2">Calories : {{ drink.calories }}</p>
+                    <p class="calories mb-2 pl-2">{{ $t('calories')}}: {{ drink.calories }}</p>
                   </div>
                   <div class="col-6">
                     <button @click='addToCart(drink)' v-on:click="toast($t('bagel.' + drink.id + '.title'))">Add</button>
@@ -53,6 +53,7 @@
                 :footer="$t('Allergy free :' + setAllergy(topping.allergyfree))"
                 class="text-center">
             <p class="card-text">{{ $t('topping.' + topping.id + '.description') }}</p>
+            <p class="calories mb-2 pl-2">{{ $t('calories')}}: {{ topping.calories }}</p>
           </b-card>
         </div>
       </div>
