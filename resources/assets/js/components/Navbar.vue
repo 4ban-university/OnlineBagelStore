c29690d85ae01390165faa0c0b1762be75e81332
Kaushik Samanta <kaushiksamanta23@gmail.com>
2018-08-12 15:14:39 -0400

    Welcome page rewamped ui

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
     <div class="container">
       <router-link :to="{ name: 'welcome' }" class="navbar-brand">
         {{ appName }}
